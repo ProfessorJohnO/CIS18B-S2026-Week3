3a3e32782de6af7db9f12d3389de9a391424cb1c
llacidrainll <gdgarcia1209@gmail.com>
2026-03-07 02:25:15 +0000

    Finished Part A

--- src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/MenuItem.java
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/MenuItem.java
@@ -3,10 +3,14 @@ class MenuItem {
     String name;
     String sku;
     BigDecimal price;
-    MenuItem(String name, String sku, BigDecimal price,) {
-        if(name == null)
-            throw 
-
+    MenuItem(String name, String sku, BigDecimal price) {
+        if(name == null||name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null or blank");}
+        if(sku == null||sku.isBlank()) {
+            throw new IllegalArgumentException("SKU cannot be null or blank");}
+        if (price == null || price.compareTo(BigDecimal.ZERO) < 0) {
+            throw new IllegalArgumentException("Price must be greater than or equal to zero");
+        }
 
 
         this.name = name;
