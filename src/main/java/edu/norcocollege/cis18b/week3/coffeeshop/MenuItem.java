eced49bce4954d36dccdd591adb2c0cd8fc2703e
llacidrainll <gdgarcia1209@gmail.com>
2026-03-07 02:11:44 +0000

    Started on Part A making MenuItem Class

---
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/MenuItem.java
@@ -0,0 +1,14 @@
+import java.math.BigDecimal;
+class MenuItem {
+    String name;
+    String sku;
+    BigDecimal price;
+    MenuItem(String name, String sku, BigDecimal price,) {
+        if(name == null)
+            throw 
+
+
+
+        this.name = name;
+        this.sku = sku;
+        this.price = price;}}
