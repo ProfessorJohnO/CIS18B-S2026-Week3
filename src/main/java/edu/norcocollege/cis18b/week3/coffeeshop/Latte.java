db52deb178ffe647c37670dd1e1637d897774004
llacidrainll <gdgarcia1209@gmail.com>
2026-03-08 23:32:02 +0000

    fixed Latte class

--- src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/Latte.java
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/Latte.java
@@ -2,13 +2,19 @@ package edu.norco.cis18b.week3.coffeeshop;
 import java.math.BigDecimal;
 public class Latte extends Beverage {
     public Latte(String sku, Size size) {
-        super(sku,"Latte",
-            BigDecimal.valueOf(4.50).multiply(
-                size == Size.SMALL ? BigDecimal.valueOf(1.00) :
-                size == Size.MEDIUM ? BigDecimal.valueOf(1.20) :
-                BigDecimal.valueOf(1.40)
-            ),
-            size
-        );
+        super(sku,"Latte",BigDecimal.valueOf(4.50).multiply(getMultiplier(size)), size);
+    }
+
+    private static BigDecimal getMultiplier(Size size) {
+        switch (size) {
+            case SMALL:
+                return BigDecimal.valueOf(1.00);
+            case MEDIUM:
+                return BigDecimal.valueOf(1.20);
+            case LARGE:
+                return BigDecimal.valueOf(1.40);
+            default:
+                return BigDecimal.ONE;
+        }
     }
 }
