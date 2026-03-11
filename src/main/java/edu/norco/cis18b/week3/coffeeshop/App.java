a5f3186b9671a77e93ca86efac3fa3212f30b538
Daniel Rios <danieljrm100873@gmail.com>
2026-03-11 05:44:31 +0000

    Updated the packages for test parts 1-4, because there were package mismatches which didn't reflect the README package wanted. Also updated: ScoreReportTest.java and pom.xml to reflect the accurate packages so I can build and run my code aside from the test part aspects. And lastly, I made a simple App.java so I can see the code build and run with mvn compile exec:java -Prun-app

--- src/main/java/edu/norco/cis18b/week3/coffeeshop/App.java
+++ src/main/java/edu/norco/cis18b/week3/coffeeshop/App.java
@@ -3,7 +3,8 @@ package edu.norco.cis18b.week3.coffeeshop; // Fixed package for entire section
 /*
  * Simple entry point for the Coffee Shop application.
  * This class is not used by the tests but allows the
- * project to compile and run if needed
+ * project to compile and run if needed to see it build
+ * like README suggests.
 */
 public class App 
 {
