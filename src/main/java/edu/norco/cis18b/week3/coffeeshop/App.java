bf883e20f5b4d42682aae71bc02f4f5df26635fa
Daniel Rios <danieljrm100873@gmail.com>
2026-03-11 05:00:13 +0000

    Part 2: Fixed the package path and implement Beverage, Latte, and ColdBrew

---
+++ src/main/java/edu/norco/cis18b/week3/coffeeshop/App.java
@@ -0,0 +1,16 @@
+package edu.norco.cis18b.week3.coffeeshop; // Fixed package for entire section
+
+/*
+ * Simple entry point for the Coffee Shop application.
+ * This class is not used by the tests but allows the
+ * project to compile and run if needed
+*/
+public class App 
+{
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Coffee Shop System Running...");
+    }
+
+}
