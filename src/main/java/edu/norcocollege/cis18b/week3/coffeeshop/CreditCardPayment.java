691efdd2f0025f791c05a3ef0e2656b67e3f0324
GamingPriest <kclang02@gmail.com>
2026-03-12 02:35:47 +0000

    finished CreditCardPayment.java

---
+++ src/main/java/edu/norcocollege/cis18b/week3/coffeeshop/CreditCardPayment.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.time.Instant;
+
+public class CreditCardPayment(String last4)
+{
+    private final String last4;
+
+    public CreditCardPayment(String last4)
+    {
+        if (last4 == null || last4.length() != 4)
+        {
+            throw new IllegalArgumentException("Last 4 digits must be exactly 4 characters");
+        } 
+        this.last4 = last4;
+    }
+
+    @Override
+    public PaymentRecipt pay(String orderId, BigDecimal amount)
+    {
+        String method = "CREDIT_CARD(****" + last4 + ")";
+        return new PaymentRecipt(orderId, amount, method, Instant.now());
+    }
+}
